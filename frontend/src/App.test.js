ceeac59b0195536ef8053b3a48949bc5af46ce43
Sayantan Hore <sayantan.hore.86@gmail.com>
2018-08-05 22:42:53 +0300

    App.test.js updated to include Provider

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const htmlStr = `
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  `;
+  ReactDOM.render(htmlStr, div);
   ReactDOM.unmountComponentAtNode(div);
 });
